83576aab454e36cc09f6af69de4ee97147547f6c
zhaoshulin <zhaoshulin.cn@gmail.com>
2014-06-05 22:38:07 +0800

    first commit

---
+++ first_try.rb
@@ -0,0 +1,38 @@
+# 第一次尝试
+
+#输出字符串到终端
+puts "Hello, Ruby!"
+
+#附加字符串
+puts "Hello, " + "Ruby " + "again!"
+puts "Hello, " << "Ruby!"
+
+#执行操作系统命令system
+system "echo 'Hello, Ruby!'"
+system "echo", "Hello,", "Ruby!"
+
+#倍乘
+puts "Hello, Ruby!" * 3
+3.times {print "Hello, Ruby!"}
+puts "Hello, " * 3 + "Ruby!"
+
+#插入shell命令
+puts "Hey guys, I am running " + `ruby --version`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
