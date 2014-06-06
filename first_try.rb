9229fc4c9ca8ad181289c111fe4812ca9811708b
Zhao Shulin <zhaoshulin.cn@gmail.com>
2014-06-06 08:43:26 +0800

    6th commit

--- first_try.rb
+++ first_try.rb
@@ -1,25 +1,28 @@
-# µÚÒ»´Î³¢ÊÔ
+#¿¿¿¿¿
 
-#Êä³ö×Ö·û´®µ½ÖÕ¶Ë
+#¿¿¿¿¿¿¿¿
 puts "Hello, Ruby!"
 
-#¸½¼Ó×Ö·û´®
+#¿¿¿¿¿
 puts "Hello, " + "Ruby " + "again!"
 puts "Hello, " << "Ruby!"
 
-#Ö´ÐÐ²Ù×÷ÏµÍ³ÃüÁîsystem
+#¿¿¿¿¿¿¿¿system
 system "echo 'Hello, Ruby!'"
 system "echo", "Hello,", "Ruby!"
 
-#±¶³Ë
+#¿¿
 puts "Hello, Ruby!" * 3
 3.times {print "Hello, Ruby!"}
 puts "Hello, " * 3 + "Ruby!"
 
-#²åÈëshellÃüÁî
+#¿¿shell¿¿
 puts "Hey guys, I am running " + `ruby --version`
 
-
+#¿¿¿¿
+hi = "Hello, "
+name = "zsl."
+puts hi + name
 
 
 
